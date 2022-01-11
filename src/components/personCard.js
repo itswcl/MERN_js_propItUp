14576fa7690c273a8a595d9feb6af4e2d0a11f5f
weicheng lee <z82111712@gmail.com>
2022-01-10 22:27:19 -0800

    add birthday function by setState to incremente age by 1 on each click

--- src/components/personCard.js
+++ src/components/personCard.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    birthday = e => {
+        // setState to update age with new age by this.state.age + 1
+        this.setState({age: parseInt(this.state.age) + 1})
+    }
+
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
         return (
 
             <div>
                 <h1>{firstName}, {lastName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.birthday}>Birthday Button for {firstName}</button>
             </div>
 
         )
