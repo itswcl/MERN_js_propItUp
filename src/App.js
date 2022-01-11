c5f099ab04258d50a129ce77b58630c6c6b48e29
weicheng lee <z82111712@gmail.com>
2022-01-10 22:44:46 -0800

    reactor app.js for people list

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import PersonCard from './components/personCard';
 
+const people = [
+  {"firstName": "Doe", "lastName": "Jane", "age":45, "hairColor":"Black"},
+  {"firstName": "Smith", "lastName": "John", "age":88, "hairColor":"Brown"},
+  {"firstName": "Fillmore", "lastName": "Millard", "age":50, "hairColor":"Brown"},
+  {"firstName": "Smith", "lastName": "Maria", "age":62, "hairColor":"Brown"},
+]
+
 function App() {
   return (
     <div className="container">
-      <PersonCard firstName={"Doe"} lastName={"Jane"} age={"45"} hairColor={"Black"}/>
-      <PersonCard firstName={"Smith"} lastName={"John"} age={"88"} hairColor={"Brown"}/>
+
+      {
+        people.map( person => {
+          return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>
+        })
+      }
+      {/* <PersonCard firstName={"Smith"} lastName={"John"} age={"88"} hairColor={"Brown"}/>
       <PersonCard firstName={"Fillmore"} lastName={"Millard"} age={"50"} hairColor={"Brown"}/>
-      <PersonCard firstName={"Smith"} lastName={"Maria"} age={"62"} hairColor={"Brown"}/>
+      <PersonCard firstName={"Smith"} lastName={"Maria"} age={"62"} hairColor={"Brown"}/> */}
     </div>
   );
 }
